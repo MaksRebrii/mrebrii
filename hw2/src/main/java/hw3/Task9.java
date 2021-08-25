10ef8c1945c46f4a91f93780bf277bba66822e79
Maksymilian Rebrii <maksim.rebriy@gmail.com>
2021-08-25 21:23:59 +0300

    complete 9th task

---
+++ hw2/src/main/java/hw3/Task9.java
@@ -0,0 +1,40 @@
+package hw3;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Task9 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        int sizeArray = -1;
+        int minValue, counterMinValue = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        while (sizeArray < 1) {
+            System.out.print("Enter array's size: ");
+            sizeArray = scanner.nextInt();
+        }
+        //array's initialization
+        for (int i = 0; i < sizeArray; i++) {
+            arrayList.add((int) (Math.random() * 100));
+        }
+
+        minValue = arrayList.get(0);
+
+        //finding minimal value and it's amount
+        for (int value :
+                arrayList) {
+            if (value < minValue)
+                minValue = value;
+            else if (value == minValue)
+                counterMinValue++;
+        }
+
+        System.out.println("Original array : " + arrayList);
+        for (int i = 0; i < counterMinValue; i++) {
+            System.out.print(minValue);
+            if(i == counterMinValue -1 ) System.out.print(".");
+            else System.out.print(", ");
+        }
+    }
+}

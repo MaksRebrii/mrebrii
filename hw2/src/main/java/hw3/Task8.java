d5751f208fe16c2dc33fc74879c119814c2e8b40
Maksymilian Rebrii <maksim.rebriy@gmail.com>
2021-08-25 21:01:50 +0300

    complete 8rd task

---
+++ hw2/src/main/java/hw3/Task8.java
@@ -0,0 +1,20 @@
+package hw3;
+
+import java.util.ArrayList;
+
+public class Task8 {
+    public static void main(String[] args) {
+        ArrayList<Integer> oddNumbers = new ArrayList<>();
+        int i = 1;
+        while (oddNumbers.size() != 10) {
+            if (i % 2 == 1) oddNumbers.add(i);
+            i++;
+        }
+        for (i = 0; i < oddNumbers.size(); i++) {
+            System.out.print(oddNumbers.get(i));
+            if(i == oddNumbers.size() -1 ) System.out.print(".");
+            else System.out.print(", ");
+        }
+    }
+}
+

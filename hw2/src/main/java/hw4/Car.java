744124a92b8a48b4ece88e6d9440743c125ffcb9
Maksymilian Rebrii <maksim.rebriy@gmail.com>
2021-08-28 15:30:29 +0300

    write functional of class Car

--- hw2/src/main/java/hw4/Car.java
+++ hw2/src/main/java/hw4/Car.java
@@ -8,9 +8,32 @@ public class Car {
     public Car(double tankVolume, double fuelInTank, double fuelConsumption) {
         this.tankVolume = tankVolume;
         this.fuelInTank = fuelInTank;
-        this.fuelConsumption = fuelConsumption;
+        setFuelConsumption(fuelConsumption);
     }
 
+    public double fillUpTheTank() {
+        double filledFuel = tankVolume - fuelInTank;
+        this.fuelInTank = tankVolume;
+        return filledFuel;
+    }
 
+    public double getFuelInTankAfterNKilometers(int numberOfKilometers) {
+        return fuelInTank - fuelConsumptionPerNKilometers(numberOfKilometers);
+    }
 
+    public double fuelNeededToFillUpToGoNKilometers(int numberOfKilometers) {
+        double fuelNeeded = Math.ceil(fuelInTank - fuelConsumptionPerNKilometers(numberOfKilometers));
+        if (fuelNeeded > 0)
+            return fuelNeeded;
+        else
+            return 0d;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption * 0.01;
+    }
+
+    private double fuelConsumptionPerNKilometers(int numberOfKilometers) {
+        return this.fuelConsumption * numberOfKilometers;
+    }
 }

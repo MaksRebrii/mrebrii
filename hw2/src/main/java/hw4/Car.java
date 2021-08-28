5e1127e13c5d3ee9641ad4fcdbb1d2e5ae31c7aa
Maksymilian Rebrii <maksim.rebriy@gmail.com>
2021-08-28 13:07:36 +0300

    create class Car

---
+++ hw2/src/main/java/hw4/Car.java
@@ -0,0 +1,16 @@
+package hw4;
+
+public class Car {
+    private final double tankVolume;
+    private double fuelInTank;
+    private double fuelConsumption;
+
+    public Car(double tankVolume, double fuelInTank, double fuelConsumption) {
+        this.tankVolume = tankVolume;
+        this.fuelInTank = fuelInTank;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+
+
+}

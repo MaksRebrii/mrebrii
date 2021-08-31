65ea57777432ad0c72a85c01ede1ccd1573a1305
Maksymilian Rebrii <maksim.rebriy@gmail.com>
2021-08-31 18:57:42 +0300

    end task4

--- hw2/src/main/java/hw4/Car.java
+++ hw2/src/main/java/hw4/Car.java
@@ -1,11 +1,11 @@
 package hw4;
 
 public class Car {
-    private final double tankVolume;
+    private final int tankVolume;
     private double fuelInTank;
     private double fuelConsumption;
 
-    public Car(double tankVolume, double fuelInTank, double fuelConsumption) {
+    public Car(int tankVolume, double fuelInTank, double fuelConsumption) {
         this.tankVolume = tankVolume;
         this.fuelInTank = fuelInTank;
         setFuelConsumption(fuelConsumption);
@@ -17,20 +17,24 @@ public class Car {
         return filledFuel;
     }
 
-    public double getFuelInTankAfterNKilometers(int numberOfKilometers) {
-        return fuelInTank - fuelConsumptionPerNKilometers(numberOfKilometers);
+    public double FuelInTankAfterNKilometers(int numberOfKilometers) {
+        fuelInTank -= fuelConsumptionPerNKilometers(numberOfKilometers);
+        return fuelInTank;
     }
 
-    public double fuelNeededToFillUpToGoNKilometers(int numberOfKilometers) {
-        double fuelNeeded = Math.ceil(fuelInTank - fuelConsumptionPerNKilometers(numberOfKilometers));
-        if (fuelNeeded > 0)
-            return fuelNeeded;
+    public double fuelNeededToFillUpAfterNKilometers(int numberOfKilometers) {
+        if (FuelInTankAfterNKilometers(numberOfKilometers) < 0)
+            return Math.abs(FuelInTankAfterNKilometers(numberOfKilometers));
         else
-            return 0d;
+            return 0;
     }
 
     public void setFuelConsumption(double fuelConsumption) {
         this.fuelConsumption = fuelConsumption * 0.01;
+    }
+
+    public double getFuelInTank() {
+        return fuelInTank;
     }
 
     private double fuelConsumptionPerNKilometers(int numberOfKilometers) {
--- hw2/src/main/java/hw4/TestCar.java
+++ hw2/src/main/java/hw4/TestCar.java
@@ -6,11 +6,27 @@ public class TestCar {
     private static final int DISTANCE_ZACHKOW_KIEV = 146;
     private static long fuelPricePerLiter;
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Car myCar = new Car(45, 20, 7d);
+        double amountOfFuel = 0d;
+        setFuelPricePerLiter(30.24);
+
+        amountOfFuel += myCar.fillUpTheTank();
+        myCar.FuelInTankAfterNKilometers(DISTANCE_ODESSA_KRIVOE_OZERO);
+        amountOfFuel += myCar.fillUpTheTank();
+        myCar.FuelInTankAfterNKilometers(DISTANCE_KRIVOE_OZERO_ZACHKOW);
+        amountOfFuel += myCar.fillUpTheTank();
+        myCar.FuelInTankAfterNKilometers(DISTANCE_ZACHKOW_KIEV);
+        System.out.printf("The cost of travel: %.2f\nFuel remaining: %.2f", amountOfFuel * getFuelPricePerLiter() / 1000, myCar.getFuelInTank());
+    }
+
+
+    public static double getFuelPricePerLiter() {
+        return fuelPricePerLiter;
     }
 
     public static void setFuelPricePerLiter(double fuelPricePerLiter) {
-        TestCar.fuelPricePerLiter = (long) (fuelPricePerLiter * 100);
+        TestCar.fuelPricePerLiter = (long) (fuelPricePerLiter * 1000);
     }
 }

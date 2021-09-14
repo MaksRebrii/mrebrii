ca00a87b3741d1c747adcc80ef60e89a6cdf1e92
Maksymilian Rebrii <maksim.rebriy@gmail.com>
2021-09-14 12:26:52 +0300

    final fix output

--- hw2/src/main/java/hw6/DepositCalculatorToRefactor.java
+++ hw2/src/main/java/hw6/DepositCalculatorToRefactor.java
@@ -14,27 +14,30 @@ public class DepositCalculatorToRefactor {
         System.out.print("Длительность вклада (лет): ");
         int term = sc.nextInt();
         System.out.println();
-        String year;
 
         for (int i = 1; i <= term; i++) {
-            BigDecimal y = sum.divide(BigDecimal.valueOf(100)).multiply(percent) ;
+            BigDecimal y = sum.divide(BigDecimal.valueOf(100)).multiply(percent).setScale(2, RoundingMode.HALF_UP) ;
             sum = sum.add(y);
-            switch (i){
-                case 1:
-                    year = "год";
-                    break;
-                case 2:
-                case 3:
-                case 4:
-                    year = "года";
-                    break;
-                default:
-                    year = "лет";
-                    break;
 
-            }
-            System.out.printf("Накопленная сумма за %d %s составила: %s грн%n", i, year, sum.setScale(2, RoundingMode.HALF_UP));
-            System.out.printf("Начисленные проценты в грн: %s%n%n", y.setScale(2, RoundingMode.HALF_UP));
+            System.out.printf("Накопленная сумма за %d %s составила: %s грн%n", i, getYear(i), sum);
+            System.out.printf("Начисленные проценты в грн: %s%n", y);
+            if (i < term)
+                System.out.println();
         }
+    }
+
+    private static String getYear(int i){
+        int lastNumber = i % 10;
+        String year;
+        boolean isExclusion = (i % 100 >= 11) && (i % 100 <= 14);
+        if (lastNumber == 0 || lastNumber >= 5)
+            year = "лет";
+        else if (isExclusion)
+            year = "лет";
+        else if (lastNumber == 1)
+            year = "год";
+        else
+            year = "года";
+        return year;
     }
 }

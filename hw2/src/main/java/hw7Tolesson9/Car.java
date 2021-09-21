35efd274c00b62e56868801e507985d657f794e3
Maksymilian Rebrii <maksim.rebriy@gmail.com>
2021-09-21 12:24:47 +0300

    comlete task

---
+++ hw2/src/main/java/hw7Tolesson9/Car.java
@@ -0,0 +1,98 @@
+package hw7Tolesson9;
+
+public class Car {
+    private static final int MIN_TANK_CAPACITY = 30;
+    private static final int MAX_TANK_CAPACITY = 60;
+    private static final double MIN_FUEL_IN_TANK_COEFFICIENT = 0.1;
+    private static final double MAX_FUEL_IN_TANK_COEFFICIENT = 0.5;
+    private static final int MONEY_COEFFICIENT = 10000;
+
+    private final int tankCapacity;
+    private final double fuelConsumption;
+    private int tripDistance;
+    private long tripMoneySpent;
+    private double fuelInTank;
+    private String location;
+
+
+    public Car(String location) {
+        this.tripDistance = (int) (Math.random() * 990 + 10);
+        this.tankCapacity = (int) (Math.random() * (MAX_TANK_CAPACITY - MIN_TANK_CAPACITY) + MIN_TANK_CAPACITY);
+        this.fuelInTank = generateFuelIntTank(tankCapacity);
+
+        this.fuelConsumption = Math.random() * 20 + 5;
+        this.location = location;
+    }
+
+    public void setTripDistanceToZero() {
+        this.tripDistance = 0;
+    }
+
+
+    public void fillFullTank(double fuelPrice) {
+        fillTank(getMaxFuelToFill(), fuelPrice);
+    }
+
+    public void drive(int distance, String destination) {
+        System.out.printf("Едем в %s. %d км...%n" , destination, distance);
+        fuelInTank -= fuelConsumptionPerNKilometers(distance);
+        tripDistance += distance;
+        if (fuelInTank < 0) throw new RuntimeException("Упс... Мы заглохли. Закончилось топливо!");
+        this.location = destination;
+    }
+
+    public void fillTank(double liters, double fuelPrice) {
+        if(liters >= getMaxFuelToFill()) {
+            tripMoneySpent += (long) (getMaxFuelToFill() * fuelPrice * MONEY_COEFFICIENT);
+            this.fuelInTank = getMaxFuelToFill();
+        }
+
+        else {
+            tripMoneySpent += (long) (liters * fuelPrice * MONEY_COEFFICIENT);
+            this.fuelInTank += liters;
+        }
+    }
+
+    public void printTripReport() {
+        System.out.println("----------Отчет о поездке----------");
+        System.out.printf("Остаток топлива в баке %.2f л. Всего проехали %d км. Всего потрачено %.2f грн. Местоположение авто - %s", getFuelInTank(), tripDistance, getTripMoneySpent(), location);
+    }
+
+
+    public double getFuelInTank() {
+        return fuelInTank;
+    }
+
+    private double generateFuelIntTank(int tankCapacity) {
+        return Math.random() * tankCapacity * (MAX_FUEL_IN_TANK_COEFFICIENT - MIN_FUEL_IN_TANK_COEFFICIENT) + tankCapacity * MIN_FUEL_IN_TANK_COEFFICIENT;
+    }
+
+    private double fuelConsumptionPerNKilometers(int numberOfKilometers) {
+        return this.fuelConsumption * numberOfKilometers / 100;
+    }
+
+    private double getMaxFuelToFill(){
+        return tankCapacity - fuelInTank;
+    }
+
+    private double getTripMoneySpent(){
+        return (double) tripMoneySpent / MONEY_COEFFICIENT;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "tripDistance=" + tripDistance +
+                ", tripMoneySpent=" + getTripMoneySpent() +
+                ", fuelInTank=" + fuelInTank +
+                ", tankCapacity=" + tankCapacity +
+                ", fuelConsumption=" + fuelConsumption +
+                ", location='" + location + '\'' +
+                '}';
+    }
+
+
+}
---
+++ hw2/src/main/java/hw7Tolesson9/TestCar.java
@@ -0,0 +1,57 @@
+package hw7Tolesson9;
+
+import java.util.Scanner;
+
+public class TestCar {
+    public static void main(String[] args) {
+        testCar();
+    }
+
+    private static void testCar() {
+        Scanner sc = new Scanner(System.in);
+        double liters;
+        double fuelPrice;
+
+        Car car = new Car("Odessa");
+        System.out.println(car);
+        car.setTripDistanceToZero();
+
+        System.out.printf("Одесса. В баке сейчас %.0f литров%n", car.getFuelInTank());
+        fuelPrice = setFuelPrice(sc);
+        car.fillFullTank(fuelPrice);
+        car.drive(179, "Кривое Озеро");
+
+        System.out.printf("Остановка Кривое Озеро. В баке сейчас %.0f литров.%n",  car.getFuelInTank());
+        liters = setFuelAmount(sc);
+        fuelPrice = setFuelPrice(sc);
+        car.fillTank(liters, fuelPrice);
+        car.drive(153, "Жашков");
+
+        System.out.printf("Остановка Жашков. В баке сейчас %.0f литров. Сколько литров заправляем? \n", car.getFuelInTank());
+        liters = setFuelAmount(sc);
+        fuelPrice = setFuelPrice(sc);
+        car.fillTank(liters, fuelPrice);
+        car.drive(150, "Киев");
+
+        car.printTripReport();
+    }
+
+    private static double setFuelPrice(Scanner scanner){
+        double fuelPrice;
+        do {
+            System.out.printf("Какая цена топлива?%n");
+            fuelPrice = scanner.nextDouble();
+        }
+        while (fuelPrice <= 0 );
+        return fuelPrice;
+    }
+
+    private static double setFuelAmount(Scanner scanner){
+        double liters;
+        do{
+            System.out.printf("Сколько литров заправляем?%n");
+            liters = scanner.nextInt();
+        }while (liters < 0);
+        return liters;
+    }
+}
